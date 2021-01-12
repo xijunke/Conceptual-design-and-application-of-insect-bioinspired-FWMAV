20d097d735633262d25f9ee936380cc8658d5197
xijunke <tianma_1014@163.com>
2021-01-12 15:39:04 +0800

    some new!

--- Chapter8/PiezoelectricActuator_for_FWMAV/DisplacementTestingOutput/PizoelectricActuator_DisplacementTestingOutput.m
+++
@@ -1,7 +0,0 @@
-% pizoelectric_actuator_displacement_output experimentally mearured by KEYENCE close range laser displacement sensor
-delta_t=xlsread('1dis.xls','A4:B1003');                 %
-t=delta_t(:,1);
-delta=delta_t(:,2);
-plot(t,delta,'r-o','LineWidth',2)
-xlabel('t')
-ylabel('\delta_{actuator}')
